f4268c110882c44bb6ba5ff5f07cc20ee2e9b8a1
markcarway <markcarway@marks-imac.lan>
2024-10-22 11:58:22 -0400

    first commit today into MarkC branch

--- SeleniumZillowTestFramework/src/test/java/tests/GoogleSearchTest.java
+++ SeleniumZillowTestFramework/src/test/java/tests/GoogleSearchTest.java
@@ -16,6 +16,7 @@ public class GoogleSearchTest {
 	private static WebDriver driver;
 
 	public static void main(String[] args) {
+		//New comment o n 10/22 @ 1200.
 
 		WebDriverManager.chromedriver().driverVersion("2.36").setup();
 		driver=new ChromeDriver();
