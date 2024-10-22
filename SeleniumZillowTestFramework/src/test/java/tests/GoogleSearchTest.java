0f948805a04be5f280ec6c5be19587dd04de0f2f
markcarway <markcarway@marks-imac.lan>
2024-10-22 12:14:20 -0400

    changed comment time

--- SeleniumZillowTestFramework/src/test/java/tests/GoogleSearchTest.java
+++ SeleniumZillowTestFramework/src/test/java/tests/GoogleSearchTest.java
@@ -16,7 +16,7 @@ public class GoogleSearchTest {
 	private static WebDriver driver;
 
 	public static void main(String[] args) {
-		//New comment on 10/22 @ 1200.
+		//New comment on 10/22 @ 1213.
 
 		WebDriverManager.chromedriver().driverVersion("2.36").setup();
 		driver=new ChromeDriver();
